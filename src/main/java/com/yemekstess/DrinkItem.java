c001c0f60961784ec2dcdef98ca30bb3ce3763b1
şeyma nur <seymanur@seyma-MacBook-Air.local>
2025-12-29 21:50:18 +0300

    Improve DrinkItem display output

--- src/main/java/com/yemekstess/DrinkItem.java
+++ src/main/java/com/yemekstess/DrinkItem.java
@@ -5,11 +5,16 @@ public class DrinkItem extends MenuItem {
     private String size;
 
     public DrinkItem(String name, double price, String size) {
-        super(name + " (" + size + ")", price);
+        super(name, price);
         this.size = size;
     }
 
     public String getSize() {
         return size;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " (" + size + ") - " + getPrice() + " TL";
     }
 }
